216d6ab5620d9ffd22d7c593971083e395bc9193
caotavio <caotavio@hotmail.com>
2020-04-28 22:26:16 +0100

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDSX_dw73XiyMDXcI_5iw1L7jAJNbPHLus',
+    authDomain: 'tavgram-ff7b6.firebaseapp.com',
+    databaseURL: 'https://tavgram-ff7b6.firebaseio.com',
+    projectId: 'tavgram-ff7b6',
+    storageBucket: 'tavgram-ff7b6.appspot.com',
+    messagingSenderId: '1098140023580',
+    appId: '1:1098140023580:web:5ee09cb5522cc2c82718d9'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDSX_dw73XiyMDXcI_5iw1L7jAJNbPHLus',
+    authDomain: 'tavgram-ff7b6.firebaseapp.com',
+    databaseURL: 'https://tavgram-ff7b6.firebaseio.com',
+    projectId: 'tavgram-ff7b6',
+    storageBucket: 'tavgram-ff7b6.appspot.com',
+    messagingSenderId: '1098140023580',
+    appId: '1:1098140023580:web:5ee09cb5522cc2c82718d9'
+  }
 };
 
 /*
